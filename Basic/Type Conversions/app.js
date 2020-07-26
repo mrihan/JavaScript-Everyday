1b6ebbbf27e939f1222ec2140cf6038ad31230af
hasan <mdmrihan@gmail.com>
2020-07-26 10:33:34 +0600

    DataType Conversions

---
+++ Basic/Type Conversions/app.js
@@ -0,0 +1,27 @@
+let example;
+
+// Number to Stering
+example = String(555);
+//example = 5+5;
+example = String(5+5);
+
+example = String (5555);
+
+//Date to String
+//example = new Date ();
+example = String (new Date());
+
+// Boolean to String
+example = String(true);
+
+// Arrey to String
+example = String([1, 2, 3,]);
+
+// Another way with toString() method;
+example = (555); toString();
+
+
+// Output
+console.log(example);
+console.log(typeof example);
+console.log(example.length);
