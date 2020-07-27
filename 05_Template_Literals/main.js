aca6729bb64b2ac9f78900185edcb54ed52ecbe2
hasan <mdmrihan@gmail.com>
2020-07-27 09:13:35 +0600

    Update Text Formate and Template Literals

---
+++ 05_Template_Literals/main.js
@@ -0,0 +1,57 @@
+/*
+let userName = prompt ("What is your name?");
+let userNameUpperCase = userName.toLowerCase();
+let userNameLowerCase = userName.toLowerCase();
+
+
+let fristLetter = userNameLowerCase.slice(0, 1);
+let totalLength = userName.length;
+
+alert("Hello" + fristLetter+ userNameLowerCase.slice(1,totalLength));
+
+*/
+
+/*console.log("string line one\n string line two");
+
+console.log(`String line one
+String line two`);
+
+
+const name = "Raihan";
+const age ="22";
+const job ="Developer";
+const city ="Dhaka"
+let html;
+function hello(){
+    return "Hellow"
+}
+
+/*
+// without Template string (es5)
+html = "<ul>" +
+        "<li> Name: " + name + "</li>" +
+        "<li> Age: " + age + "</li>" +
+        "<li> job: " + job +  "</li>" +
+        "<li> city: " + city +  "</li>" +
+        "</ul>"
+document.body.innerHTML = html;
+*/
+
+// with Template String
+
+/*html = `
+    <ul>
+        <li>Name: ${name}</li>
+        <li>Age: ${age}</li>
+        <li>Job: ${job}</li>
+        <li>City: ${city}</li>
+        <li>Likes: ${100+10}k</li>
+        <li>Function: ${hello()}</li>
+    </ul>
+`
+document.body.innerHTML =html;
+*/
+
+
+
+
