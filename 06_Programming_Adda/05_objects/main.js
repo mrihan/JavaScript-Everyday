dec3e914c7f4b104772717d9da529a6835c1e269
hasan <mdmrihan@gmail.com>
2020-07-29 09:14:12 +0600

    Objects and Arrays

---
+++ 06_Programming_Adda/05_objects/main.js
@@ -0,0 +1,47 @@
+// car objects
+// {} = Object literal
+//let firstName = String("Raihan"); // Factory Function
+//let lastName = new String("Raihan"); // constructor Function
+
+// Dot Notation
+// bracket notation
+
+let carBrand = "Audi";
+let carColor = "Yellow";
+let carModel = "A4";
+let carPrice = "45000";
+
+/*let car = {
+  brand: "Audi",
+  color: "Yellow",
+  model: "A",
+  price: 45000,
+};
+console.log(car);
+*/
+
+// Dot Notation
+
+/*let car = {
+  brand: "Audi",
+  color: "Yellow",
+  model: "A",
+  price: 45000,
+};
+
+*/
+// car.brand = "Tata"; Changing mode
+// console.log(car.brand);
+
+// Bracket notation
+
+let car = {
+  brand: "Audi",
+  color: "Yellow",
+  model: "A",
+  price: 45000,
+};
+//console.log(car["brand"]);
+
+let selectedOption = "color";
+console.log(car[selectedOption]);
---
+++ 06_Programming_Adda/06_Arrays/main.js
@@ -0,0 +1,33 @@
+/*
+let name = "Raihan"
+let number = 1;
+
+let car = {
+  barnd: "Audi";
+  price: 45000;
+  color:"Red";
+}
+*/
+
+/*let numbers = [1, 3, 7];
+console.log(numbers);
+
+let name = ["Manik", "Raihan", "Hasan"];
+console.log(name);
+*/
+let numbers = [1, 3, 7];
+console.log(numbers[1]);
+
+//numbers[0] = 4;
+//console.log(numbers);
+
+let cars = [
+  { barnd: "Addi", price: 45000 },
+  { barnd: "Tata", price: 65000 },
+];
+
+cars[1].barnd = { barnd: "Honda", price: 85000 };
+console.log(cars[1]);
+
+let numberoftype = [1, 3, 7];
+console.log(numberoftype.length);
