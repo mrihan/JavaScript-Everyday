2f8f2461f6d35f1f0ce8e7137bdee5b1d4e2f9ea
hasan <mdmrihan@gmail.com>
2020-08-10 18:52:09 +0600

    Template literals update

--- 05_Template_Literals/project_2.js
+++ 05_Template_Literals/project_2.js
@@ -1,28 +1,18 @@
-const name = "Hasan";
-const age  = 18;
-const job  = "Student"
-const phone = 01746330554;
-const city = "Chapai Nawabgonj";
-const favoriteColor = "white";
-const favoriteFootbolTeam = "Argentina";
-const favoriteCricketTeam = "Australia";
-function hello(){
-    return "Hello"
-}
+const name = "Rihan";
+const age = 28;
+const job = "Developer";
+const city = "Rajshahi";
 let html;
 
+// with template string (es6)
 
 html = `
-     <ul>
+    <ul>
         <li>Name: ${name}</li>
         <li>Age: ${age}</li>
         <li>Job: ${job}</li>
-        <li>Phone: ${phone}</li>
-        <li>City: ${city}</li>
-        <li>Favorite Color: ${favoriteColor}</li>
-        <li>Favorite Footbol Team: ${favoriteFootbolTeam}</li>
-        <li>Favorite Cricket Team: ${favoriteCricketTeam}</li>
-        <li>Function: ${hello}</li>
-     </ul>
-`
+        <li>city: ${city}</li>
+    </ul>
+`;
+
 document.body.innerHTML = html;
