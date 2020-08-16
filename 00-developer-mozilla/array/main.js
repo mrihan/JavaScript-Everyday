18c1856cf4ba6d475ece84cd8afd150605a0e106
hasan <mdmrihan@gmail.com>
2020-08-16 08:15:15 +0600

    Mozilla Developer Docs Example

---
+++ 00-developer-mozilla/array/main.js
@@ -0,0 +1,7 @@
+//Create an Array
+//let fruits = ["Apple", "Banana"];
+//console.log(fruits.length);
+
+//Access an Array item using the index position
+let fruits = ["Apple", "Banana"];
+let fruits = fruits[0];
