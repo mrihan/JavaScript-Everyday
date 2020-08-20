5b67d8ce18a58c2a38e219739ddee706e67d088a
hasan <mdmrihan@gmail.com>
2020-08-20 18:21:24 +0600

    variable and datatype

---
+++ 00_general/01-variable-datatype.js
@@ -0,0 +1,54 @@
+var person = "Rihan";
+console.log(person);
+var some_thing = "Anithing";
+console.log(some_thing);
+var _canI = "yes";
+console.log(_canI);
+var myName = "Rihan";
+console.log(myName);
+
+// Datatype
+var isTure = true;
+console.log(isTure);
+
+var isFals = false;
+console.log(isFals);
+
+//undefined
+var unde_fined;
+console.log(unde_fined);
+
+//null
+var some_null = null;
+console.log(some_null);
+
+var text = "hello " + "there";
+console.log(text);
+
+var welecomeMsg = "HI " + text;
+console.log(welecomeMsg);
+console.log(typeof welecomeMsg);
+
+var number = 10 + 10;
+console.log(number);
+console.log(typeof number);
+
+var isString = "hello there" + 20;
+console.log(isString);
+console.log(typeof isString);
+
+var complex = 10 + 10 + "some text " + 20; // type casting
+console.log(complex);
+console.log(typeof complex);
+
+var myName = "Abdullah";
+console.log(myName);
+var myName = "hasan";
+console.log(myName);
+
+var someThing = "Abdullah Hasan";
+console.log(someThing);
+console.log(typeof someThing);
+someThing = 10;
+console.log(someThing);
+console.log(typeof someThing);
